afe31da86ee512e8144b71d2f830f64ea2d77039
Peter Huang <huangcx22@mails.tsinghua.edu.cn>
2024-02-27 19:17:25 +0800

    Added first little thing

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,19 +3,26 @@ import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const existsCollection = defineCollection({
   type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    link: z.string(),
+    image: image(),
+    alt: z.string()
+  })
+});
+const ideasCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string()
-    }),
     tags: z.array(z.string())
   })
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   exists: existsCollection,
+  ideas: ideasCollection
 };
---
+++ src/content/exists/shot-scraper/index.md
@@ -0,0 +1,12 @@
+---
+title: 'shot-scraper'
+pubDate: 2024-02-27
+description: '一个截图用的命令行工具，兼可为爬虫'
+link: 'https://github.com/simonw/shot-scraper'
+image: './simonwillison-net-2022-Mar-14-scraping-web-pages-shot-scraper.png'
+alt: '屏幕截图'
+---
+
+这个命令行工具，既可以对给定的网站进行截图，又可以在这个网页上任意运行javascript代码，从而实现爬虫的效果。看起来是非常强力的工具！
+
+来自大名鼎鼎的[Simon Wilson](https://simonwillison.net/2022/Mar/14/scraping-web-pages-shot-scraper/)，这篇文章介绍了使用方法。后来，他还尝试把它接入了Github Action，作为自动的截图工具。
